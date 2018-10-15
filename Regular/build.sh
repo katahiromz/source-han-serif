d550c24115cfa56806751b552bae9232dddfbe0d
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-10-15 14:54:12 +0900

    improve build.sh

--- Regular/build.sh
+++ Regular/build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+if ! [ -x makeotf ]; then
+    echo 'ERROR: makeotf not found.'
+    exit 1
+fi
+
 makeotf -f cidfont.ps.JP -omitMacNames -ff features.JP -fi cidfontinfo.JP -mf ../FontMenuNameDB.SUBSET -r -nS -cs 1 -ch ../UniSourceHanSerifJP-UTF32-H -ci ../SourceHanSerif_JP_sequences.txt
