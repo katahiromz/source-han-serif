45d9e512ab050190880db498cf21f574f48c686f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-10-15 14:50:52 +0900

    add Regular/build.sh

---
+++ Regular/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+makeotf -f cidfont.ps.JP -omitMacNames -ff features.JP -fi cidfontinfo.JP -mf ../FontMenuNameDB.SUBSET -r -nS -cs 1 -ch ../UniSourceHanSerifJP-UTF32-H -ci ../SourceHanSerif_JP_sequences.txt
